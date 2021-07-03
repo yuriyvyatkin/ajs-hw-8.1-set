53c26429fd325d0bda504ae4ffdbb97118ca97f8
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-07-03 14:15:29 +1000

    Add Team.js and Character.js files

---
+++ src/js/Team.js
@@ -0,0 +1,27 @@
+export default class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(member) {
+    if (this.members.has(member)) {
+      throw new Error(`Ошибка. Объект ${member} уже добавлен!`);
+    }
+
+    this.members.add(member);
+  }
+
+  addAll(...args) {
+    args.forEach((member) => {
+      if (this.members.has(member)) {
+        throw new Error(`Ошибка. Объект ${member} уже добавлен!`);
+      }
+
+      this.add(member);
+    });
+  }
+
+  toArray() {
+    return Array.from(this.members);
+  }
+}
